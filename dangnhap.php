419f26fb3f2587e26cb3d1e02ec0346261abdb64
01652879966 <45027530+01652879966@users.noreply.github.com>
2018-12-11 22:57:42 +0700

    Add files via upload

---
+++ dangnhap.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+
+<body>
+    <div class="container">
+        <!--day la phan top-->
+        <div id="top">
+            <div id="top-left">
+                <ul>
+                    <li><a href="#"><img src="fb.png"></a></li>
+                    <li><a href="#"><img src="aaa.png"></a></li>
+                </ul>
+            </div>
+            <div id="top-right">
+                <ul>
+                    <li><img src="login.jpg" alt=""></li>
+                    <li>|</li>
+                    <li><a href="#">Đăng Nhập</a></li>
+                    <li>|</li>
+                    <li><a href="dangki.php">Đăng Kí</a></li>
+                </ul>
+            </div>
+        </div>
+        <!--day la phan header-->
+        <div id="header">
+            <div>
+                <li><a href="#"><img src="loggoo.png"></a></li>
+            </div>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="index2.html">Trang Chủ</a></li>
+                <li><a href="">Cho Thuê Phòng</a>
+                    <ul>
+                        <li><a href="#" title="TP Hồ Chí Minh">TP Hồ Chí Minh</a></li>
+                        <li><a href="#" title="Hà Nội">Hà Nội</a></li>
+                        <li><a href="#" title="Bình Dương">Bình Dương</a></li>
+                        <li><a href="#" title="Đồng Nai">Đồng Nai</a></li>
+                        <li><a href="#" title="Vũng Tàu">Vũng Tàu</a></li>
+                        <li><a href="#" title="Cần Thơ">Cần Thơ</a></li>
+                        <li><a href="#" title="Hải Phòng">Hải Phòng</a></li>
+                        <li><a href="#" title="Đà Nẵng">Đà Nẵng</a></li>
+                        <li><a href="#" title="Thừa Thiên Huế">Thừa Thiên Huế</a></li>
+
+                    </ul>
+                </li>
+                <li><a href="">Tìm Ở Ghép</a>
+                    <ul>
+                        <li><a href="#" title="TP Hồ Chí Minh">TP Hồ Chí Minh</a></li>
+                        <li><a href="#" title="Hà Nội">Hà Nội</a></li>
+                        <li><a href="#" title="Bình Dương">Bình Dương</a></li>
+                        <li><a href="#" title="Đồng Nai">Đồng Nai</a></li>
+                        <li><a href="#" title="Vũng Tàu">Vũng Tàu</a></li>
+                        <li><a href="#" title="Cần Thơ">Cần Thơ</a></li>
+                        <li><a href="#" title="Hải Phòng">Hải Phòng</a></li>
+                        <li><a href="#" title="Đà Nẵng">Đà Nẵng</a></li>
+                        <li><a href="#" title="Thừa Thiên Huế">Thừa Thiên Huế</a></li>
+
+                    </ul>
+                </li>
+                <li><a href="">Nhà Cho Thuê</a>
+                    <ul>
+                        <li><a href="#" title="TP Hồ Chí Minh">TP Hồ Chí Minh</a></li>
+                        <li><a href="#" title="Hà Nội">Hà Nội</a></li>
+                        <li><a href="#" title="Bình Dương">Bình Dương</a></li>
+                        <li><a href="#" title="Đồng Nai">Đồng Nai</a></li>
+                        <li><a href="#" title="Vũng Tàu">Vũng Tàu</a></li>
+                        <li><a href="#" title="Cần Thơ">Cần Thơ</a></li>
+                        <li><a href="#" title="Hải Phòng">Hải Phòng</a></li>
+                        <li><a href="#" title="Đà Nẵng">Đà Nẵng</a></li>
+                        <li><a href="#" title="Thừa Thiên Huế">Thừa Thiên Huế</a></li>
+
+                    </ul>
+
+                </li>
+
+                <li><a href="">Giới Thiệu</a></li>
+                <li><a href="">Hướng Dẫn </a></li>
+                <li><a href="">Đăng Tin</a></li>
+
+            </ul>
+            <div class="clear"></div>
+
+        </div>
+        <div style="margin-top: 5px;width: 960px;margin-left:auto;background: url(aaaaa.png);border-bottom-color: black;">
+            <h1>ĐĂNG NHẬP TÀI KHOẢN</h1>
+        </div>
+        <fieldset style="width:310px;margin-left:300px;height:150px;margin-top:100px;background:url(aaaaa.png)">
+    <legend style="margin-left:130px">LOGIN</legend>
+    <form action="dapnhap.php" method="post">
+        <table>
+            <tr>
+                <td>UserName</td>
+                <td><input type="text"size="25"name="txtname"/></td>
+            </tr>
+            <tr>
+                <td>PassWord</td>
+                <td><input type="password"size="25" /></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" value="LoGin"/></td>
+            </tr>
+        </table>
+    </form>
+</fieldset>
+</body>
+</html>
