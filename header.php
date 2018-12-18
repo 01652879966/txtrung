d6b661352af4f3be3e36bfb2c84e2ade7d47ef72
01652879966 <45027530+01652879966@users.noreply.github.com>
2018-12-18 21:53:31 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+
+<body>
+    <div class="container">
+        <!--day la phan top-->
+        <div id="top">
+            <div id="top-left">
+                <ul>
+                    <li><a href="#"><img src="fb.png"></a></li>
+                    <li><a href="#"><img src="aaa.png"></a></li>
+                </ul>
+            </div>
+            <div id="top-right">
+                <ul>
+                    <li><img src="login.jpg" alt=""></li>
+                    <li>|</li>
+                    <li><a href="dangnhap.php">Đăng Nhập</a></li>
+                    <li>|</li>
+                    <li><a href="dangki.php">Đăng Kí</a></li>
+                </ul>
+            </div>
+        </div>
+        <!--day la phan header-->
+        <div id="header">
+            <div>
+                <li><a href="#"><img src="loggoo.png"></a></li>
+            </div>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="index2.html">Trang Chủ</a></li>
+                <li><a href="">Cho Thuê Phòng</a>
+                    <ul>
+                        <li><a href="#" title="TP Hồ Chí Minh">TP Hồ Chí Minh</a></li>
+                        <li><a href="#" title="Hà Nội">Hà Nội</a></li>
+                        <li><a href="#" title="Bình Dương">Bình Dương</a></li>
+                        <li><a href="#" title="Đồng Nai">Đồng Nai</a></li>
+                        <li><a href="#" title="Vũng Tàu">Vũng Tàu</a></li>
+                        <li><a href="#" title="Cần Thơ">Cần Thơ</a></li>
+                        <li><a href="#" title="Hải Phòng">Hải Phòng</a></li>
+                        <li><a href="#" title="Đà Nẵng">Đà Nẵng</a></li>
+                        <li><a href="#" title="Thừa Thiên Huế">Thừa Thiên Huế</a></li>
+
+                    </ul>
+                </li>
+                <li><a href="">Tìm Ở Ghép</a>
+                    <ul>
+                        <li><a href="#" title="TP Hồ Chí Minh">TP Hồ Chí Minh</a></li>
+                        <li><a href="#" title="Hà Nội">Hà Nội</a></li>
+                        <li><a href="#" title="Bình Dương">Bình Dương</a></li>
+                        <li><a href="#" title="Đồng Nai">Đồng Nai</a></li>
+                        <li><a href="#" title="Vũng Tàu">Vũng Tàu</a></li>
+                        <li><a href="#" title="Cần Thơ">Cần Thơ</a></li>
+                        <li><a href="#" title="Hải Phòng">Hải Phòng</a></li>
+                        <li><a href="#" title="Đà Nẵng">Đà Nẵng</a></li>
+                        <li><a href="#" title="Thừa Thiên Huế">Thừa Thiên Huế</a></li>
+
+                    </ul>
+                </li>
+                <li><a href="">Nhà Cho Thuê</a>
+                    <ul>
+                        <li><a href="#" title="TP Hồ Chí Minh">TP Hồ Chí Minh</a></li>
+                        <li><a href="#" title="Hà Nội">Hà Nội</a></li>
+                        <li><a href="#" title="Bình Dương">Bình Dương</a></li>
+                        <li><a href="#" title="Đồng Nai">Đồng Nai</a></li>
+                        <li><a href="#" title="Vũng Tàu">Vũng Tàu</a></li>
+                        <li><a href="#" title="Cần Thơ">Cần Thơ</a></li>
+                        <li><a href="#" title="Hải Phòng">Hải Phòng</a></li>
+                        <li><a href="#" title="Đà Nẵng">Đà Nẵng</a></li>
+                        <li><a href="#" title="Thừa Thiên Huế">Thừa Thiên Huế</a></li>
+
+                    </ul>
+
+                </li>
+
+                <li><a href="">Giới Thiệu</a></li>
+                <li><a href="">Hướng Dẫn </a></li>
+                <li><a href="">Đăng Tin</a></li>
+
+            </ul>
+            <div class="clear"></div>
+
+        </div>
+
+
+        <!--phan doi thieu-->
+        <div class="review">
+            <h1>Website cho thuê phòng trọ, nhà trọ số 1 Việt Nam</h1>
+            <p>Cho thuê phòng trọ, nhà trọ số 1 Việt Nam - Website đăng tin cho thuê phòng trọ, tìm phòng trọ
+                nhanh, hiệu quả với hơn 90.000+ tin đăng và hơn 2.000.000 lượt xem mỗi tháng. <b>cho thuê phòng
+                    trọ, nhà trọ, thuê nhà nguyên căn, cho thuê căn hộ, tìm người ở ghép...</b> Vì vậy tin của bạn
+                đăng trên website sẽ tiếp cận với nhiều khách hàng hơn, do đó giao dịch nhanh hơn, tiết kiệm chi
+                phí hơn.</p>
+        </div>
+        
+    
+</body>
+</html>
